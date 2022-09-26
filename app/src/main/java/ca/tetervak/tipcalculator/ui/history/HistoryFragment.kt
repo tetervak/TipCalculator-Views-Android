2648a1625e14371031bc236b7dbfbb59f8e83e2c
Alex Tetervak <tetervak@sheridancollege.ca>
2022-09-26 17:52:17 -0400

    moved History fragment into the matching ui package

--- app/src/main/java/ca/tetervak/tipcalculator/ui/history/HistoryFragment.kt
+++ app/src/main/java/ca/tetervak/tipcalculator/ui/history/HistoryFragment.kt
@@ -1,4 +1,4 @@
-package ca.tetervak.tipcalculator
+package ca.tetervak.tipcalculator.ui.history
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import ca.tetervak.tipcalculator.R
 import ca.tetervak.tipcalculator.databinding.FragmentHistoryBinding
 
 /**
